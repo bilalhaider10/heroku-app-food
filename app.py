5088f6f0aed176f993cfac110eee63df25d6cb23
bilalhaider10 <muhammadbilalhaider10@gmail.com>
2022-10-03 18:36:33 +0300

    asd

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import tensorflow as tf
 model = tf.keras.models.load_model('my_model5.hdf5')
 import streamlit as st
 st.write("""
-         # Food21 image classification and Prediction
+         Food Classification
          """
          )
-st.write("This is a simple image classification web app to predict food")
+st.write("Image classification web app to predict the type food made on streamlit")
 file = st.file_uploader("Please upload an image file", type=["jpg", "png"])
 
 
@@ -33,47 +33,44 @@ else:
     prediction = import_and_predict(image, model)
     
     if np.argmax(prediction) == 0:
-        st.write("It is a apple_pie!")
+        st.write("Predicted: apple_pie!")
     elif np.argmax(prediction) == 1:
-        st.write("It is a baby_black_ribs!")
+        st.write("Predicted: baby_black_ribs!")
     elif np.argmax(prediction) == 2:
-        st.write("It is a baklava!")
+        st.write("Predicted: baklava!")
     elif np.argmax(prediction) == 3:
-        st.write("It is a beef_carpaccio!")
+        st.write("Predicted: beef_carpaccio!")
     elif np.argmax(prediction) == 4:
-        st.write("It is a beef_tartare!")
+        st.write("Predicted: beef_tartare!")
     elif np.argmax(prediction) == 5:
-        st.write("It is a beet_salad!")
+        st.write("Predicted: beet_salad!")
     elif np.argmax(prediction) == 6:
-        st.write("It is a beignets!")
+        st.write("Predicted: beignets!")
     elif np.argmax(prediction) == 7:
-        st.write("It is a bibimbap!")
+        st.write("Predicted: bibimbap!")
     elif np.argmax(prediction) == 8:
-        st.write("It is a bread_pudding!")
+        st.write("Predicted: bread_pudding!")
     elif np.argmax(prediction) == 9:
-        st.write("It is a breakfast_burrito!")
+        st.write("Predicted: breakfast_burrito!")
     elif np.argmax(prediction) == 10:
-        st.write("It is a bruschetta!")
+        st.write("Predicted: bruschetta!")
     elif np.argmax(prediction) == 11:
-        st.write("It is a caesar_salad!")
+        st.write("Predicted: caesar_salad!")
     elif np.argmax(prediction) == 12:
-        st.write("It is a cannoli!")
+        st.write("Predicted: cannoli!")
     elif np.argmax(prediction) == 13:
-        st.write("It is a caprese_salad!")
+        st.write("Predicted: caprese_salad!")
     elif np.argmax(prediction) == 14:
-        st.write("It is a carrot_cake!")
+        st.write("Predicted: carrot_cake!")
     elif np.argmax(prediction) == 15:
-        st.write("It is a ceviche!")
+        st.write("Predicted: ceviche!")
     elif np.argmax(prediction) == 16:
-        st.write("It is a cheesecake!")
+        st.write("Predicted: cheesecake!")
     elif np.argmax(prediction) == 17:
-        st.write("It is a cheese_plate!")
+        st.write("Predicted: cheese_plate!")
     elif np.argmax(prediction) == 18:
-        st.write("It is a chicken_curry!")
+        st.write("Predicted: chicken_curry!")
     elif np.argmax(prediction) == 19:
-        st.write("It is a chicken_quesadilla!")
+        st.write("Predicted: chicken_quesadilla!")
     else:
-        st.write("big class")
-    
-    st.text("Probability (0: 'apple_pie', 1: 'baby_back_ribs', 2:'baklava', 3:'beef_carpaccio',4:'beef_tartare', 5: 'beet_salad', 6:'beignets', 7:'bibimbap',8: 'bread_pudding', 9:'breakfast_burrito', 'bruschetta', 'caesar_salad', 'cannoli', 'caprese_salad', 'carrot_cake', 'ceviche', 'cheesecake','cheese_plate', 'chicken_curry', 'chicken_quesadilla', 'big_class' ")
-    st.write(prediction)
+        st.write("Predicted: other categories")
